3ec2071d51b42985fe17bf8699a4462ee56ee866
Tasha <14nlm@queensu.ca>
2018-01-30 11:03:29 -0500

    uncommented GUI

--- src/LanguageDevelopmentApp/MainApp.java
+++ src/LanguageDevelopmentApp/MainApp.java
@@ -6,14 +6,16 @@ import javax.swing.*;
 
 public class MainApp {
 
-    //private JPanel panelMain;
+    private JPanel panelMain;
+    private JTextArea languageDevelopmentApplicationTextArea;
+
 
     public static void main(String[] args) throws IOException{
-        //JFrame startUp = new JFrame("MainApp");
-        //startUp.setContentPane(new MainApp().panelMain);
-/*        startUp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame startUp = new JFrame("MainApp");
+        startUp.setContentPane(new MainApp().panelMain);
+        startUp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         startUp.pack();
-        startUp.setVisible(true);*/
+        startUp.setVisible(true);
 
         GameCard.importCard();
 
